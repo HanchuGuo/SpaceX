d3935c44ccffc30608ca63c26fae2357c6163006
hanchu <hanchu@gmail.com>
2021-09-05 10:44:14 -0400

    spacex

---
+++ starlink/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="footer">
+                    ©2020 StarLink Tracker. All Rights Reserved. Website Made by Your name
+                </footer>
+
+            </div>
+        );
+    }
+}
+
+export default Footer;
